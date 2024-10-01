60eef55664263a4758b5fcf083892681a4ef3620
mmagan <marco.magan.sanz@edu.xunta.gal>
2024-10-01 15:20:36 +0200

    commit inicial

---
+++ dump/myDb.sql
@@ -0,0 +1,26 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+CREATE TABLE `Modulo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+  `abreviatura` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `Modulo` (`id`, `nombre`, `abreviatura`) VALUES
+(1, 'Desenvolvemento Web Contorno Servidor', 'DWCS'),
+(2, 'Desenvolvemento Web Contorno Cliente', 'DWCC'),
+(3, 'Programación', 'PROG'),
+(4, 'Contornos de Desenvolvemento', 'CD');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ www/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+            
+                echo "<h1>¡Hola, Bienvenido al módulo de DWCS 2024-25!</h1>";
+            ?>
+        </div>
+    </body>
+</html>
