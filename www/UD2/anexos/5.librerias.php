4874b96756e50b6601adc06706d7388c09c2f796
Marco <marco@iessanclemente.net>
2024-10-11 12:26:06 +0200

    ud2 bol4 solucion

--- www/UD2/anexos/5.librerias.php
+++ www/UD2/anexos/5.librerias.php
@@ -1,1 +1,20 @@
+<html>
+    <head>
+    <title>DWCS UD2. Boletín 4. Solución</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h1>Anexo 5. Librerias</h1>
+            <br />
+            <h3>Tarea </h3> 
+            
+            <?php
+                
+            ?>
 
+
+        </div>  
+    </body>
+</html>
